8261c33d509724a6fcf38d6917ecc5344d32cd16
Private-Kawa <voidout76@gmail.com>
2023-02-09 17:12:50 +0900

    マイグレーション news作成

---
+++ database/migrations/2023_02_09_164501_create_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('admin_id');
+            $table->string('title', 30);
+            $table->string('content');
+            $table->date('date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+};
