6d696f5454dec9b1dd107840128cce51f8b15430
Private-Kawa <voidout76@gmail.com>
2023-02-14 21:46:55 +0900

    home.blade.php Event一覧の表示 完了

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,13 @@
 <x-app-layout>
-    
+    <h1 class="text-2xl pb-3">あなたの予定</h1>
+    <section>
+        <div class='events'>
+            @foreach ($events as $event)
+                <div class='event'>
+                    <h2 class='title'>{{ $event->title }}</h2>
+                    <p class='body'>{{ $event->content }}</p>
+                </div>
+            @endforeach
+        </div>
+    </section>
 </x-app-layout>
