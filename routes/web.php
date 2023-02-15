5da85f422abb5c800c34254cbfec61bfdddd4469
Private-Kawa <voidout76@gmail.com>
2023-02-15 16:39:14 +0900

    route 修正

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/home', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('home');
 
+Route::get('/dashboard', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
