047dfb4ef0ad17ec377d85b03be0f7093cc3777a
Private-Kawa <voidout76@gmail.com>
2023-04-10 01:01:00 +0900

    管理者の削除禁止 削除画面の日本語対応

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,18 +1,28 @@
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('アカウント削除') }}
+            アカウント削除
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('一度アカウントを削除しますと、すべての登録されているデータが失われます。') }}
+            一度アカウントを削除しますと、すべての登録されているデータが失われます。
         </p>
     </header>
+    
+    <!-- ユーザー -->
+    @if(Auth::user()->id !==  1)
+        <x-danger-button
+            x-data=""
+            x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
+        >アカウント削除</x-danger-button>
+    @else
+    <!-- 管理者 -->
+        <p class="text-md font-medium text-gray-900 dark:text-gray-100">
+            管理者アカウントは削除できません。
+        </p>
+    @endif
+    
 
-    <x-danger-button
-        x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('アカウント削除') }}</x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
@@ -20,11 +30,11 @@
             @method('delete')
 
             <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('Are you sure you want to delete your account?') }}
+                {{ __('本当にアカウントを削除してもよろしいでしょうか') }}
             </h2>
-
+            
             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                一度アカウントを削除しますと、すべての登録されているデータが失われます。
             </p>
 
             <div class="mt-6">
@@ -35,7 +45,7 @@
                     name="password"
                     type="password"
                     class="mt-1 block w-3/4"
-                    placeholder="Password"
+                    placeholder="パスワード"
                 />
 
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
@@ -43,11 +53,11 @@
 
             <div class="mt-6 flex justify-end">
                 <x-secondary-button x-on:click="$dispatch('close')">
-                    {{ __('Cancel') }}
+                    {{ __('キャンセル') }}
                 </x-secondary-button>
 
                 <x-danger-button class="ml-3">
-                    {{ __('Delete Account') }}
+                    {{ __('アカウント削除') }}
                 </x-danger-button>
             </div>
         </form>
